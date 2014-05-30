c9e6a96ce3fe5cc9ea61be62e8e1d83e3b78b4dd
puhrome <phirom.yim@fullsail.edu>
2014-05-29 21:26:27 -0400

    Return Value Functions
    Explaining what a return in a function in WebStorm

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Return Values Created by puhrome on 5/29/14.
+ */
+
+var total = calcArea(30, 20);//return function store in var total
+//functions that are returning need to be assigned a variable
+function calcArea() {//scope to calc area
+    var area = w * h;
+    return area;//function spitting info out
+}
+console.log(total);
